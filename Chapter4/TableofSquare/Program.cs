4d0035c47eaf804686d1b15d2778e386a36b5df7
Malachi <mtoomer81@gmail.com>
2014-03-26 09:15:27 -0400

    problem 7

---
+++ Chapter4/TableofSquare/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TableofSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int square;
+            for (int i = 1; i <= 20; i++)
+            {
+
+                
+
+
+                square = i * i;
+
+                Console.WriteLine("The number: {0} is squared into: {1}",i,square);
+            }
+        }
+    }
+}
